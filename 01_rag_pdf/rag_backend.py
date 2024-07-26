77778a286cb34d30d99db54920e67b2c388ef289
tosnufc <tosnufc@gmail.com>
2024-07-26 22:33:27 +0700

    fixing 01_rag_pdf

--- 01_rag_pdf/rag_backend.py
+++ 01_rag_pdf/rag_backend.py
@@ -67,58 +67,62 @@ def get_ai_response(qa, query):
 
 qa = get_standalone_question()
 
-# loop = True
-# while loop:
-#     user_query = input('Ask your question: ')
-#     if user_query != "/bye":
-#         response = get_ai_response(qa=qa, query=user_query)
-#         print('')
-#         print(response)
-#         print('')
-#     else:
-#         loop = False
+loop = True
+while loop:
+    user_query = input('Ask your question: ')
+    if user_query != "/bye":
+        response = get_ai_response(qa=qa, query=user_query)
+        print('')
+        print(response)
+        print('')
+    else:
+        loop = False
 
 
+# Example of questions
+
+# Who are Notion’s LLM Providers?
+# What is FN74085?
 
 
 
 # Setup Tkinter GUI
-root = Tk()
-root.title('Ask Me')
+# root = Tk()
+# root.title('Ask Me')
 
-def retrieve_input():
-    inputValue = textBox.get("1.0", END).strip()
-    get_ai_response(qa, query=inputValue)
-    textBox.delete("1.0", END)
-    Output.insert(END, f"YOU:  {inputValue}\n\n", 'user')
-    Output.insert(END, f"AI-BOT:  {answer}\n\n", 'bot')
-    rOutput.insert(END, f"Question: {inputValue} --> {db_query}\n", 'query')
-    for doc in db_response:
-        rOutput.insert(END, f"File: {doc.metadata['source']}\nPage: {doc.metadata['page']}\n", 'source')
-        rOutput.insert(END, f"{doc.page_content[:500]}.......\n", 'content')
+# def retrieve_input():
+#     inputValue = textBox.get("1.0", END).strip()
+#     get_ai_response(qa, query=inputValue)
+#     textBox.delete("1.0", END)
+#     Output.insert(END, f"YOU:  {inputValue}\n\n", 'user')
+#     Output.insert(END, f"AI-BOT:  {answer}\n\n", 'bot')
+#     rOutput.insert(END, f"Question: {inputValue} --> {db_query}\n", 'query')
+#     for doc in db_response:
+#         rOutput.insert(END, f"File: {doc.metadata['source']}\nPage: {doc.metadata['page']}\n", 'source')
+#         rOutput.insert(END, f"{doc.page_content[:500]}.......\n", 'content')
 
-# Setup Tkinter GUI elements
-textBox = Text(root, height=5, width=65, font=('Helvetica', 12), wrap=WORD)
-textBox.grid(row=1, column=0, padx=10, pady=10)
+# # Setup Tkinter GUI elements
+# textBox = Text(root, height=5, width=65, font=('Helvetica', 12), wrap=WORD)
+# textBox.grid(row=1, column=0, padx=10, pady=10)
 
-buttonCommit = ttk.Button(root, text="Chat", command=retrieve_input)
-buttonCommit.grid(row=1, column=1, padx=10, pady=10)
+# buttonCommit = ttk.Button(root, text="Chat", command=retrieve_input)
+# buttonCommit.grid(row=1, column=1, padx=10, pady=10)
 
-scrollbar = Scrollbar(root)
-Output = Text(root, height=20, width=65, wrap=WORD, yscrollcommand=scrollbar.set, font=('Helvetica', 12))
-Output.grid(row=0, column=0, padx=10, pady=10)
-Output.tag_configure('user', foreground='#3333FF')
-Output.tag_configure('bot', foreground='#FF5733')
-scrollbar.grid(row=0, column=1, sticky='ns')
-scrollbar.config(command=Output.yview)
+# scrollbar = Scrollbar(root)
+# Output = Text(root, height=20, width=65, wrap=WORD, yscrollcommand=scrollbar.set, font=('Helvetica', 12))
+# Output.grid(row=0, column=0, padx=10, pady=10)
+# Output.tag_configure('user', foreground='#3333FF')
+# Output.tag_configure('bot', foreground='#FF5733')
+# scrollbar.grid(row=0, column=1, sticky='ns')
+# scrollbar.config(command=Output.yview)
 
-rscrollbar = Scrollbar(root)
-rOutput = Text(root, height=12, width=65, wrap=WORD, yscrollcommand=rscrollbar.set, font=('Helvetica', 12))
-rOutput.grid(row=0, column=2, padx=10, pady=10, sticky='ns')
-rOutput.tag_configure('query', foreground='#3333FF')
-rOutput.tag_configure('source', foreground='#FF5733')
-rscrollbar.grid(row=0, column=3, sticky='ns')
-rscrollbar.config(command=rOutput.yview)
+# rscrollbar = Scrollbar(root)
+# rOutput = Text(root, height=12, width=65, wrap=WORD, yscrollcommand=rscrollbar.set, font=('Helvetica', 12))
+# rOutput.grid(row=0, column=2, padx=10, pady=10, sticky='ns')
+# rOutput.tag_configure('query', foreground='#3333FF')
+# rOutput.tag_configure('source', foreground='#FF5733')
+# rscrollbar.grid(row=0, column=3, sticky='ns')
+# rscrollbar.config(command=rOutput.yview)
 
-# Run Tkinter event loop
-root.mainloop()
+# # Run Tkinter event loop
+# root.mainloop()
